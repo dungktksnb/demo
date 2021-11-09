fd2a0deabcd2509299ababec9fb8d8843d72ef90
dungktksnb <81791502+dungktksnb@users.noreply.github.com>
2021-11-09 10:14:46 +0700

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppRouterRoutingModule} from './app-router-routing.module';
+import {EditComponent} from './edit/edit.component';
+import {DeleteComponent} from './delete/delete.component';
+import {CreateComponent} from './create/create.component';
+import {ListComponent} from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditComponent,
+    DeleteComponent,
+    CreateComponent,
+    ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AppRouterRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
